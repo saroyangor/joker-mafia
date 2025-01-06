81e942176e6b9818f02bc42cbbe59680df702240
saroyangor <dr.saroyan98@gmail.com>
2025-01-06 20:52:15 +0400

    address fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ const mobileLinks = [
 const links = [
   {
     'title': 'О нас',
-    'link': '/about'
+    'link': '/about',
   },
   {
     'title': 'Услуги',
@@ -64,13 +64,13 @@ export function Hero({allRefs, heroRef}) {
   const [opened, setOpened] = useState(false)
 
   const handleClick = (link) => {
-    if(link === 'hero') {
+    if (link === 'hero') {
       // eslint-disable-next-line react/prop-types
-      heroRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+      heroRef.current?.scrollIntoView({behavior: 'smooth', block: 'center'})
     } else {
       const block = link.split('/')[1]
       // eslint-disable-next-line react/prop-types
-      allRefs[block].current?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+      allRefs[block].current?.scrollIntoView({behavior: 'smooth', block: 'center'})
     }
 
     setOpened(false)
@@ -84,15 +84,14 @@ export function Hero({allRefs, heroRef}) {
         </h1>
         <p className="lg:text-4xl sm:text-2xl text-xl lh-125">
           <span className="text-red font-bold">Спортивная мафия</span> в самом центре Еревана для начинающих и опытных
-          мафиози. <span className="text-red font-medium">Играем 5 раз в неделю</span> в lounge bar "The Babylon" по адресу
+          мафиози. <span className="text-red font-medium">Играем 5 раз в неделю</span> в lounge bar "The Babylon" по
+          адресу
           Терьяна 59.
         </p>
-        <a
-          href="https://t.me/Mafclubjoker"
-          target="_blank"
-          className="lg:text-3xl sm:text-xl text-lg bg-red text-white sm:p-6 p-3 rounded-full lg:mt-24 sm:mt-12 mt-4 hover:bg-white hover:text-red transition duration-300">Записаться
-          на игру
-        </a>
+        <button
+          className="lg:text-3xl sm:text-xl text-lg bg-red text-white sm:p-6 p-3 rounded-full lg:mt-24 sm:mt-12 mt-4 hover:bg-white hover:text-red transition duration-300">
+          <a href="https://t.me/Mafclubjoker" target="_blank"> Записаться на игру</a>
+        </button>
       </div>
       <div
         className="lg:hidden flex justify-between px-4 w-full items-center sm:h-36 h-24 fixed top-0 left-0 z-50 bg-black bg-opacity-90">
